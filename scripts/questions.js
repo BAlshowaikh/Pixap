2758208c2327aa8494e779d345d20f7b37eb3cc6
BAlshowaikh <batoolalshowaikh04@outlook.com>
2025-10-04 21:23:07 +0300

    Added logic to handle run code button clicks

--- scripts/questions.js
+++ scripts/questions.js
@@ -3,6 +3,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const levelId = url.get("level")
   let currentStepIndex = 0
   const dots = document.querySelectorAll(".q-dot")
+  const messageOverlay = document.querySelector("#message-overlay")
+  const messageText = document.querySelector("#message-text")
+  const runCodeBtn = document.getElementById("run-code-btn")
+  const scoreHTML = document.getElementById("score")
+  const codeInput = document.getElementById("code-input")
+  let score = 0
 
   // This will hold the level info once it has been fetched
   let levelData = null
@@ -33,17 +39,13 @@ document.addEventListener("DOMContentLoaded", () => {
       // If no errors found, update html
       const loadStepContent = (clickedDotIndex) => {
         const step = levelData[clickedDotIndex]
-        console.log("This is from loadStepContent function")
-        console.log(`Level data for ${clickedDotIndex}`)
-        console.log(`Data ${step} `)
         document.getElementById("question-title").textContent = step.title
         document.getElementById("instruction-text").textContent =
           step.instruction
         document.getElementById("code-input").value = step.initial_code
         dots.forEach((dot) => {
-          dot.title = `Click to jump to ${step.index+1}.`
+          dot.title = `Click to jump to ${step.index + 1}.`
         })
-
       }
 
       // Function to manage the dots
@@ -53,6 +55,72 @@ document.addEventListener("DOMContentLoaded", () => {
           dot.classList.remove("active")
         })
         dots[clickedDotIndex].classList.add("active")
+      }
+
+      // Define a function to handle the automatic flow of questions once "Run code" is clicked
+      const advanceStep = () => {
+        if (currentStepIndex < levelData.length - 1) {
+          currentStepIndex++
+          loadStepContent(currentStepIndex)
+          updateNavigationDots(currentStepIndex)
+        } else {
+          displayMessage("Level completed!", true)
+          window.location.href = "../map.html"
+        }
+      }
+
+      // Define a function to show the feedback
+      const displayMessage = (message, isSuccess) => {
+        console.log("inside display message ")
+        messageOverlay.classList.remove("d-none")
+        messageText.textContent = message
+
+        if (isSuccess) {
+          score += 10
+          scoreHTML.textContent = ` ${score} PX`
+          // Show for 20 seconds
+          setTimeout(() => {
+            messageOverlay.classList.add("d-none")
+            advanceStep()
+          }, 5000)
+        } else {
+          // In case of wrong answer, hide the overlay div
+          setTimeout(() => {
+            messageOverlay.classList.add("d-none")
+          }, 2000)
+        }
+      }
+
+      // Define a function to handle the code checker
+      const checkUserCode = () => {
+        const step = levelData[currentStepIndex]
+        console.log("inside checkUserCode ")
+        console.log(currentStepIndex)
+        // If it's not a question, just continue
+        if (step.type !== "question") return
+
+        // Retrive the user code and level expect code and pass them to be normalized before comaprison
+        const userCode = normalizeCode(codeInput.value)
+        const expectedCode = normalizeCode(step.expected_code)
+        // Check if the user code is equal to the correct question code
+        if (userCode == expectedCode) {
+          displayMessage(step.run_output_message || "Success! Well done.", true)
+        } else {
+          displayMessage("Try again :(", false)
+        }
+      }
+
+      // Function to normalize the codes (user code or json code)
+      const normalizeCode = (code) => {
+        let newCode = code.toLowerCase()
+        // The 'g' flag ensures ALL instances are replaced, not just the first one. and the \n targets a newline character. and \r means that the cursor will be in the start of the current line.
+        newCode = newCode.replace(/(\r\n|\n|\r)/g, " ")
+
+        // Collapse multiple whitespace characters (spaces, tabs) into a single space
+        // The \s+ regex matches one or more whitespace characters.
+        newCode = newCode.replace(/\s+/g, " ")
+
+        return newCode.trim()
       }
 
       // Call the functions to load the very first step (index 0)
@@ -66,6 +134,11 @@ document.addEventListener("DOMContentLoaded", () => {
           loadStepContent(currentStepIndex)
           updateNavigationDots(currentStepIndex)
         })
+      })
+
+      // Add eventlistener to the run code button
+      runCodeBtn.addEventListener("click", () => {
+        checkUserCode()
       })
     })
 })
